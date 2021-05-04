3837beb8eefb6bdc22d166d6f99c8e7a9d393545
Kenny Tran <kenny.tran138@gmail.com>
2021-05-04 08:53:43 -0500

    Initial Commit, Repo Transfer

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const compression = require('compression');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(compression());
+app.use(logger('tiny'));
+// possibly express.raw
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = app;
